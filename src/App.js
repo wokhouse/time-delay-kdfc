87443640e3ce159a7dac04527166af14d3340cc2
Jade Fung <jade@fungj.net>
2022-11-15 14:24:36 -0800

    styles very pretty

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='vh-100 d-flex align-items-center justify-content-center'>
+      <div>
+      📻 radio time
+      </div>
     </div>
   );
 }
