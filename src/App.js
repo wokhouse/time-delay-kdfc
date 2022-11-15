c6faa3020ee98058033c6cfd1934992b2a424b4f
Jade Fung <jade@fungj.net>
2022-11-15 14:13:06 -0800

    new CRA

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Player from './Player';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="App container mt-1">
-      <h1>KDFC synced</h1>
-      <Player/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
